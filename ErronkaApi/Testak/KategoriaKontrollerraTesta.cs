119bb6d1ea0f9399f80ed41766d4355a37238841
Alain Afonso Redondo <alainafonsoredondo@gmail.com>
2026-03-13 16:50:06 +0100

    KategoriaKontrollerraTesta igota

---
+++ ErronkaApi/Testak/KategoriaKontrollerraTesta.cs
@@ -0,0 +1,14 @@
+﻿using ErronkaApi.DTOak;
+using ErronkaApi.Repositorioak;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace ErronkaApi.Kontrollerrak
+{
+   
+    public class KategoriaKontrollerraTesta
+    {
+        
+    }
+}
